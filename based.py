18ff0d3d9f382148f4f2b759a5c1aa0f515a4b9d
hwipl <33433250+hwipl@users.noreply.github.com>
2019-03-21 12:41:21 +0100

    add based.py
    Add first version of nuqql-based.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ based.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python3
+
+import socketserver
+import argparse
+import pathlib
+import daemon
+import sys
+import os
+
+
+class NuqqlBaseHandler(socketserver.BaseRequestHandler):
+    """
+    Request Handler for the server, instantiated once per client connection.
+
+    This is limited to one client connection at a time. It should be fine for
+    our basic use case.
+    """
+
+    buffer = b""
+
+    def handle(self):
+        # self.request is the client socket
+        while True:
+            self.data = self.request.recv(1024)
+            # self.buffer += self.data.decode()
+            self.buffer += self.data
+
+            # try to find a complete message
+            eom = self.buffer.find(b"\r\n")
+            if eom == -1:
+                continue
+
+            # extract message from buffer
+            msg = self.buffer[:eom]
+            self.buffer = self.buffer[eom + 2:]
+
+            # start message handling
+            try:
+                msg = msg.decode()
+            except UnicodeDecodeError:
+                # invalid message format, drop client
+                return
+            handleMsg(msg)
+
+            # print msg and send it back to client
+            # print(msg)
+            # msg = msg + "\r\n"
+            # self.request.sendall(msg.encode())
+
+
+def handleAccountList():
+    """
+    List all accounts
+    """
+
+    print("account list")
+
+
+def handleAccountAdd(params):
+    """
+    Add a new account.
+    """
+
+    print("account add")
+
+
+def handleAccountBuddies(account, params):
+    """
+    Get buddies for a specific account. If params contains "online", filter
+    online buddies.
+    """
+
+    if len(params) >= 1 and params[0].lower() == "online":
+        online = "online"
+    else:
+        online = "all"
+
+    print("account {0}: get {1} buddies".format(account, online))
+
+
+# def handleAccount(parts, account, command, params):
+def handleAccount(parts):
+    """
+    Handle account specific commands received from client
+    """
+
+    if parts[1] == "list":
+        # special case for "list" command
+        command = parts[1]
+    elif parts[1] == "add":
+        # special case for "add" command
+        command = parts[1]
+        params = parts[2:]
+    elif len(parts) >= 3:
+        # account specific commands
+        account = parts[1]
+        command = parts[2]
+        params = parts[3:]
+    else:
+        # invalid command, ignore
+        return
+
+    if command == "list":
+        return handleAccountList()
+
+    if command == "add":
+        # TODO: currently this supports
+        # "account <ID> add" and "account add <ID>", OK?
+        return handleAccountAdd(params)
+
+    if command == "buddies":
+        return handleAccountBuddies(account, params)
+
+
+def handleMsg(msg):
+    """
+    Handle messages received from client
+    """
+
+    # get parts of message
+    parts = msg.split()
+
+    # account specific commands
+    if len(parts) >= 2 and parts[0] == "account":
+        handleAccount(parts)
+
+    # others
+    # TODO: ver? who?
+    # ignore rest for now...
+
+
+def runInetServer(args):
+    """
+    Run an AF_INET server
+    """
+
+    listen = (args.address, args.port)
+    with socketserver.TCPServer(listen, NuqqlBaseHandler) as server:
+        server.serve_forever()
+    return
+
+
+def runUnixServer(args):
+    """
+    Run an AF_UNIX server
+    """
+
+    # make sure paths exist
+    pathlib.Path(args.dir).mkdir(parents=True, exist_ok=True)
+    sockfile = args.dir + "/" + args.sockfile
+    try:
+        # unlink sockfile of previous execution of the server
+        os.unlink(sockfile)
+    except FileNotFoundError:
+        # ignore if the file did not exist
+        pass
+    with socketserver.UnixStreamServer(sockfile, NuqqlBaseHandler) as server:
+        server.serve_forever()
+
+
+def runServer(args):
+    """
+    Run the server; can be AF_INET or AF_UNIX.
+    """
+
+    # AF_INET
+    if args.af == "inet":
+        if args.daemonize:
+            # daemonize the server
+            with daemon.DaemonContext():
+                runInetServer(args)
+        else:
+            # run in foreground
+            runInetServer(args)
+
+    # AF_UNIX
+    elif args.af == "unix":
+        if args.daemonize:
+            # daemonize the server
+            with daemon.DaemonContext():
+                runUnixServer(args)
+        else:
+            # run in foreground
+            runUnixServer(args)
+    return
+
+
+def getCommandLineArgs():
+    """
+    Parse the command line and return command line arguments:
+        af:         address family
+        address:    AF_INET listen address
+        port:       AF_INET listen port
+        sockfile:   AF_UNIX listen socket file within working directory
+        dir:        working directory
+        daemonize:  daemonize process?
+    """
+
+    # parse command line parameters
+    parser = argparse.ArgumentParser(description="Run a basic nuqql daemon.")
+    parser.add_argument("--af", choices=["inet", "unix"], default="inet",
+                        help="socket address family: \"inet\" for AF_INET, \
+                        \"unix\" for AF_UNIX")
+    parser.add_argument("--address", default="localhost",
+                        help="AF_INET listen address")
+    parser.add_argument("--port", default=32000, help="AF_INET listen port")
+    parser.add_argument("--sockfile", default="based.sock",
+                        help="AF_UNIX socket file in DIR")
+    parser.add_argument("--dir", default=os.getcwd() + "/nuqql-based",
+                        help="working directory")
+    parser.add_argument("-d", "--daemonize", action="store_true",
+                        help="daemonize process (default: true)")
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+    # start server
+    try:
+        runServer(getCommandLineArgs())
+    except KeyboardInterrupt:
+        sys.exit()
