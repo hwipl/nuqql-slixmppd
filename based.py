6c65cb9c3b0fdc7a4d290162a72f505240e53db3
hwipl <33433250+hwipl@users.noreply.github.com>
2019-09-29 16:53:46 +0200

    add "help" command
    Add the "help" command for listing all commands and their description.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- based.py
+++ based.py
@@ -229,6 +229,50 @@ class Format(str, Enum):
     CHAT_LIST = "chat: list: {0} {1} {2} {3}" + EOM
     CHAT_MSG = "chat: msg: {0} {1} {2} {3} {4}" + EOM
 
+    # help message
+    HELP_MSG = """info: List of commands and their description:
+account list
+    list all accounts and their account ids.
+account add <protocol> <user> <password>
+    add a new account for chat protocol <protocol> with user name <user> and
+    the password <password>. The supported chat protocol(s) are backend
+    specific. The user name is chat protocol specific. An account id is
+    assigned to the account that can be shown with "account list".
+account <id> delete
+    delete the account with the account id <id>.
+account <id> buddies [online]
+    list all buddies on the account with the account id <id>. Optionally, show
+    only online buddies with the extra parameter "online".
+account <id> collect
+    collect all messages received on the account with the account id <id>.
+account <id> send <user> <msg>
+    send a message to the user <user> on the account with the account id <id>.
+account <id> status get
+    get the status of the account with the account id <id>.
+account <id> status set <status>
+    set the status of the account with the account id <id> to <status>.
+account <id> chat list
+    list all group chats on the account with the account id <id>.
+account <id> chat join <chat>
+    join the group chat <chat> on the account with the account id <id>.
+account <id> chat part <chat>
+    leave the group chat <chat> on the account with the account id <id>.
+account <id> chat send <chat> <msg>
+    send the message <msg> to the group chat <chat> on the account with the
+    account id <id>.
+account <id> chat users <chat>
+    list the users in the group chat <chat> on the account with the
+    account id <id>.
+account <id> chat invite <chat> <user>
+    invite the user <user> to the group chat <chat> on the account with the
+    account id <id>.
+bye
+    disconnect from backend
+quit
+    quit backend
+help
+    show this help""" + EOM
+
     def __str__(self):
         return str(self.value)
 
@@ -615,6 +659,10 @@ def handle_msg(msg):
             if parts[0] == "quit":
                 callback(acc.aid, Callback.QUIT, ())
         return (parts[0], "Goodbye.")
+
+    # handle "help" command
+    if parts[0] == "help":
+        return ("msg", Format.HELP_MSG)
 
     # others
     # TODO: ver? who?
