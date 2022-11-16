b1685d963ade31aa78e953a584917b52844fdad8
hwipl <33433250+hwipl@users.noreply.github.com>
2022-11-16 09:42:50 +0100

    update slixmpp to v1.8.3
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -61,5 +61,5 @@ setup(
     },
     classifiers=CLASSIFIERS,
     python_requires='>=3.7',
-    install_requires=["nuqql-based~=0.3.0", "slixmpp~=1.8.2"],
+    install_requires=["nuqql-based~=0.3.0", "slixmpp~=1.8.3"],
 )
